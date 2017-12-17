4399a885840d302c177d27a10f84d805dba4997c
Eric Singer <git@ericsinger.com>
2017-12-16 19:33:43 -0500

    Color thingy

--- Assets/scripts/new_Builder.cs
+++ Assets/scripts/new_Builder.cs
@@ -42,10 +42,11 @@ public class new_Builder : MonoBehaviour {
 			// set prefab transforms
 			stepClone.transform.localPosition = new Vector3(x,y,z);
 			stepClone.transform.localScale = new Vector3 (length,depth,width);
-			stepClone.transform.RotateAround(Vector3.zero, Vector3.forward, rotationDegrees);
+			stepClone.transform.RotateAround(Vector3.zero, Vector3.forward, rotationDegrees + 90.0f);
 			//stepClone.transform.RotateAround(Vector3.zero, Vector3.forward, -90);
 
-			Color myColor = new Vector4 ( (float) n / (float) pNumBars,(float) n / (float) pNumBars, 1.0f, 1.0f);
+			Color myColor = Color.HSVToRGB((float) n / (float) pNumBars, 0.7f, 1.0f);
+			//Color myColor = new Vector4 ( (float) n / (float) pNumBars,(float) n / (float) pNumBars, 1.0f, 1.0f);
 			stepClone.GetComponent<Renderer>().material.color = myColor;
 
 				
