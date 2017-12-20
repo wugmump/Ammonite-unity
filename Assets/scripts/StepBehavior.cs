1c170ec3d614475585cb2825737d9595312fb0dd
wugmump <wugmump@mac.com>
2017-12-19 23:00:48 -0500

    broken but still interesting
    almost got it spitting out notes

--- Assets/scripts/StepBehavior.cs
+++ Assets/scripts/StepBehavior.cs
@@ -2,26 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
 public class StepBehavior : MonoBehaviour {
+
+	public IPEndPoint remoteEndPoint;
+	public UdpClient client;
+	public byte[] sendStr;
 
 	public int StepNum;
 
 	// Use this for initialization
 	void Start () {
-		
+		//Debug.Log("Sending packets");
+		remoteEndPoint = new IPEndPoint (IPAddress.Parse ("127.0.0.1"), 6076);
+		client = new UdpClient ();
+		sendStr = new byte[256];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// let's check for collisions
+
 	}
 
-	public int setStepNumber (int whichStep) {
+	void OnCollisionEnter(Collision collision){
+		Debug.Log ("BOOM!");
+		playNote ();
+	}
+
+
+	public void setStepNumber (int whichStep) {
 	
 		// called by the new builder script when the step is generated
 		StepNum = whichStep;
-		return whichStep;
 	
 	}
 
+	public void playNote() {
+
+		sendStr = Encoding.UTF8.GetBytes("note/" + StepNum);
+		client.Send(sendStr, sendStr.Length, remoteEndPoint);
+	}
 }
--- Assets/scripts/ballFaller.cs
+++ Assets/scripts/ballFaller.cs
@@ -16,23 +16,31 @@ public class ballFaller : MonoBehaviour {
 	public byte[] sendStr;
 
 	void Start () {
-		Debug.Log("Sending packets");
-
-		remoteEndPoint = new IPEndPoint(IPAddress.Parse("10.10.10.103"), 6076);
-		client = new UdpClient();
-
-		sendStr = new byte[256];
+//		Debug.Log("Sending packets");
+//
+//		remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6076);
+//		client = new UdpClient();
+//
+//		sendStr = new byte[256];
 	}
 
+
+	void onCollisionEnter(Collision collision) {
+		// does this work?
+		Debug.Log("Collision event at step");
+
+	}
+
+
 	// Update is called once per frame
-	void Update () {
-		var myNote = transform.position.y;
-		if (myNote >= 1) {
-			sendStr = Encoding.UTF8.GetBytes("collision " + myNote);
-			client.Send(sendStr, sendStr.Length, remoteEndPoint);
-		}
+//	void Update () {
+//		var myNote = transform.position.y;
+//		if (myNote >= 1) {
+//			sendStr = Encoding.UTF8.GetBytes("note/" + myNote);
+//			client.Send(sendStr, sendStr.Length, remoteEndPoint);
+//		}
 
 
 		
-	}
+
 }
