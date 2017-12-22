58e3110a56eb319e2da7ff75394edcc149f5adf7
wugmump <wugmump@mac.com>
2017-12-21 22:03:42 -0500

    unfucking git
    ballfaller degitted

--- Assets/scripts/ballFaller.cs
+++ Assets/scripts/ballFaller.cs
@@ -22,39 +22,27 @@ public class ballFaller : MonoBehaviour {
 //		client = new UdpClient();
 //
 //		sendStr = new byte[256];
+
+		remoteEndPoint = new IPEndPoint (IPAddress.Parse ("127.0.0.1"), 6076);
+		client = new UdpClient ();
+
 	}
 
-
-<<<<<<< HEAD
-		remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6076);
-		client = new UdpClient();
-=======
 	void onCollisionEnter(Collision collision) {
 		// does this work?
 		Debug.Log("Collision event at step");
->>>>>>> 3cae1a6660b92e7d40a132d143b6d7472d0a0a8c
 
 	}
 
 
 	// Update is called once per frame
-<<<<<<< HEAD
 	void Update () {
 		var myNote = transform.position.y;
 		//if (myNote >= 1) {
 			sendStr = Encoding.UTF8.GetBytes("collision " + myNote);
 			client.Send(sendStr, sendStr.Length, remoteEndPoint);
-		//}
-=======
-//	void Update () {
-//		var myNote = transform.position.y;
-//		if (myNote >= 1) {
-//			sendStr = Encoding.UTF8.GetBytes("note/" + myNote);
-//			client.Send(sendStr, sendStr.Length, remoteEndPoint);
-//		}
->>>>>>> 3cae1a6660b92e7d40a132d143b6d7472d0a0a8c
 
 
-		
+	}
 
 }
